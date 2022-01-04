390407e6967d6c32fe1ddaf7800b1575a1b02dd2
Damian Barczak <db303189@student.polsl.pl>
2022-01-04 06:52:45 +0100

    zmiana w projekcie 2

--- src/pl/java/Main.java
+++ src/pl/java/Main.java
@@ -3,6 +3,7 @@ package pl.java;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello - Umieść mnie na GitHub'ie");
+
+        System.out.println("Hello a- Umieść mnie na GitHub'ie");
     }
 }
